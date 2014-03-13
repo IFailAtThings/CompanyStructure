4b03c7f9951e0fdde2b679753f2aa6b5fcc5f7ee
Harry Hamblin <hhamblin3@gmail.com>
2014-03-13 14:44:48 +0000

    Added Manager Class and variables, get set commands

---
+++ src/Manager.java
@@ -0,0 +1,26 @@
+
+
+public class Manager extends CompanyPerson {
+		 public Employee[] employees;
+		 private int numEmployees = 0;
+		 
+		 public Employee[] getEmployees() {
+			 return employees;
+		 }
+		 
+		 public void addEmployee(Employee thisemployee){
+			 employees[numEmployees] = thisemployee;
+			 numEmployees ++;
+			 		 }
+
+		 public void removeEmployee(int id){
+			 for(int i = 0; i < numEmployees; i ++){
+				 if(employees[i].getId() == id){
+					 for(int j = i; j < numEmployees; j ++){
+						 employees[j] = employees[j+1];
+					 }
+					 numEmployees--;
+				 }
+			 }
+		 }
+}
